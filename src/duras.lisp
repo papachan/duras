7b2a3b2b671b432f1fb0027acdb2a6c23eef33d2
papachan <danloaiza@yahoo.com>
2015-11-07 10:31:47 -0500

    running tests

--- src/duras.lisp
+++ src/duras.lisp
@@ -1,12 +1,17 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
-
 ;;;; duras.lisp
+
 (in-package :cl-user)
-(defpackage #:duras
-  (:use #:cl)
+
+(defpackage :duras
+  (:use :cl)
   (:export :hello))
-(in-package #:duras)
+
+
+(in-package :duras)
+
+;;; "duras" goes here. Hacks and glory await!
 
 (defun hello()
-  (print "hello duras"))
-;; 
+  (print "hello example test"))
+;;
