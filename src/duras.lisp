9792e6d3654258a5c5d79cfc68ab7b7700e6e392
papachan <danloaiza@yahoo.com>
2015-11-08 20:12:49 -0500

    adding some tests

--- src/duras.lisp
+++ src/duras.lisp
@@ -1,17 +1,34 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
 ;;;; duras.lisp
+;(do-all-symbols (sym (find-package :duras)) (export sym))
 
 (in-package :cl-user)
 
 (defpackage :duras
   (:use :cl)
-  (:export :hello))
-
+  (:documentation "a simple exercise"))
 
 (in-package :duras)
 
 ;;; "duras" goes here. Hacks and glory await!
 
 (defun hello()
-  (print "hello example test"))
+  (print "hello example test")
+  (terpri))
+
+; a range function
+(defun range (min max)
+  (loop for n from min below (+ 1 max)
+       collect n))
+
+;;; interleaves two lists in a method:
+;;; (range 1 6) (:1 :2 :3 :4 :5 :6)
+(defparameter *mylist* '(:1 :2 :3 :4 :5 :6))
+
+(defun interleave (lst1 lst2)
+  "interleave example"
+  (cond ((and (eql lst1 nil) (eql lst2 nil)) nil)
+        ((eql lst1 nil ) (cons nil (interleave lst2 lst1)))
+        (t  (cons (first lst1) (interleave lst2 (rest lst1))))))
+
 ;;
--- t/duras-test.lisp
+++ t/duras-test.lisp
@@ -1,21 +1,18 @@
 ;;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
-
 (asdf:oos 'asdf:load-op :fiveam)
 
 (defpackage duras-test
-  (:use :cl :fiveam :duras))
+  (:use :cl :fiveam :duras)
+  (:import-from :duras :hello))
 
 (in-package :duras-test)
 
 ; test a call a function from duras
-(duras:hello)
+(hello)
 
 ;;;; First we need some functions to test.
 
-(defun range (start end)
-  (if (> start end)
-      nil
-      (cons start (range (+ start 1) end))))
+(defparameter *mylist* '(:1 :2 :3 :4 :5 :6))
 
 (defun add-2 (n)
   (+ n 2))
@@ -29,22 +26,26 @@
   (is (= 2 (add-2 0)))
   (is (= 0 (add-2 -2))))
 
-(run 'add-2) 
-
 (explain! (run 'add-2))
-
-(run! 'add-2)
-
-(def-suite :duras-test :description "first tests")
-
-(in-suite :duras-test)
 
 (test ok-test
   (is (= (+ 2 2) 4)))
 
+(run 'ok-test)
+
 (test range-2
   "Test the RANGE function"
-  (is (equal '(1 2 3 4 5 6) (range 1 6))))
+  (is (equal '(1 2 3 4 5 6) (duras::range 1 6))))
+
+(explain! (run 'range-2))
+
+(test interleaves-test
+  "Test an interleave function with two lists"
+  (is (equal '(1 :1 2 :2 3 :3 4 :4 5 :5 6 :6)
+             (duras::interleave (duras::range 1 6) *mylist*))))
+
+(explain! (run 'interleaves-test))
 
 
-(run! 'duras-test)
+;; 
+
