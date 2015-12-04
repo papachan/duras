e8fc52d5d38f29c5f51e8e1e6a6aa8119158bde1
Dan Loaiza <papachan@gmail.com>
2015-12-03 19:55:46 -0500

    update tests

--- t/duras-test.lisp
+++ t/duras-test.lisp
@@ -14,66 +14,45 @@
 
 (defparameter *mylist* '(:1 :2 :3 :4 :5 :6))
 
-(defun add-2 (n)
-  (+ n 2))
-
-(defun add-4 (n) 
-  (+ n 4))
-
 (def-suite duras-suite :description "first tests")
 
 (in-suite duras-suite)
 
-(test add-2
-      "Test the ADD-2 function"
-      (is (= 2 (add-2 0)))
-      (is (= 0 (add-2 -2))))
-
-(run! 'duras-suite)
-
 ;; ++++++++++++++++++++++++++++++++++++++++++
-(test ok-test
-      (is (= (+ 2 2) 4)))
-
-;; range test
-(test ok-range
-      "Test the RANGE function"
-      (is (equal '(1 2 3 4 5 6) (duras::range 1 6)))
-      (is (equal '(1 2) (duras::range 1 2))))
+(test range-test
+  "Test the RANGE function"
+  (is (equal '(1 2) (duras::range 1 2)))
+  (is (equal '(2 3 4 5 6) (duras::range 2 6)))
+  (is (equal '(-2 -1 0 1 2 3) (duras::range -2 3))))
 
 (run! 'duras-suite)
 
 ;; ++++++++++++++++++++++++++++++++++++++++++
 ;; average test
-(test ok-average
-      "Test an Average function"
-      (is (equal 3 (duras::average '(0 6))))
-      (is (equal 5/2 (duras::average '(0 1 2 3 4 5)))))
-
-(run! 'duras-suite)
-
-;; ++++++++++++++++++++++++++++++++++++++++++
-(test interleaves-test
-      "Test an interleave function with two lists"
-      (is (equal '(1 :1 2 :2 3 :3 4 :4 5 :5 6 :6)
-                 (duras::interleave (duras::range 1 6) *mylist*))))
+(test average-test
+  "Test an Average function"
+  (is (equal 3 (duras::average '(0 6))))
+  (is (equal 5/2 (duras::average '(0 1 2 3 4 5)))))
 
 (run! 'duras-suite)
 
 ;; ++++++++++++++++++++++++++++++++++++++++++
 (test flatten-test
-      "Test a flatten list"
-      (is (equal '(1 2 3 4) (duras::flatten '(1 (2) (3) 4)))))
+  "Test a flatten list"
+  (is (equal '(1 2 3 4) (duras::flatten '(1 (2) (3) 4))))
+  (is (equal '(1 2 3 4 5 6) (duras::flatten '(1 2 (3 4 5 6))))))
 
 (run! 'duras-suite)
 
 ;; ++++++++++++++++++++++++++++++++++++++++++
 (test find-min-max
-      "Test which find the min and the max of a list"
-      (is (equal '(0 10) (duras::find-min-max '(1 0 10 4)))))
+  "Test which find the min and the max of a list"
+  (is (equal '(0 4) (duras::find-min-max '(1 0 4))))
+  (is (equal '(0 10) (duras::find-min-max '(1 0 10 4)))))
 
 (run! 'duras-suite)
 
+;; ++++++++++++++++++++++++++++++++++++++++++
 (test smallest-two
   "A test finding the smallest two values at a list"
   (is (equal '(1 2)  (duras::smallest-two '(6 5 4 3 2 1 3 4 5 6 7)))) 
@@ -83,11 +62,23 @@
 
 (run! 'duras-suite)
 
-(test calculate-list-exp
+;; ++++++++++++++++++++++++++++++++++++++++++
+(test calculate-list-sum-exp
   "A test to calculate the sum of digits of 2**n and sum"
-  (is (equal 26 (duras::calculate-list-exp 15))))
+  (is (equal 4 (duras::calculate-list-exp 2)))
+  (is (equal 26 (duras::calculate-list-exp 15)))
+  (is (equal 256 (duras::calculate-list-exp 200))))
 
 (run! 'duras-suite)
+
+;; ++++++++++++++++++++++++++++++++++++++++++
+(test interleaves-test
+  "Test an interleave function with two lists"
+  (is (equal '(1 :1 2 :2 3 :3 4 :4 5 :5 6 :6)
+             (duras::interleave (duras::range 1 6) *mylist*))))
+
+(run! 'duras-suite)
+
 
 
 
